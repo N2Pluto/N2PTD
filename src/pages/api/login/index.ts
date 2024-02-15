1937b2313b9f909996d3acf1d4f0b3173f8f06c4
n4sunday <dev.nattapon.s@gmail.com>
2024-02-15 22:26:45 +0700

    feat: add demo login

--- src/pages/api/login/index.ts
+++ src/pages/api/login/index.ts
@@ -1,37 +1,77 @@
 import supabase from 'src/libs/supabase'
+import jwt from 'jsonwebtoken'
 
 const handler = async (req: any, res: any) => {
-  const { email_student_id, password } = req.body
+  try {
+    if (req.method !== 'POST') {
+      return res.status(405).json({ message: 'Method Not Allowed' })
+    }
 
-  // First, try to find a user with matching student_id
-  let { data, error } = await supabase
-    .from('Users')
-    .select('student_id,email,password')
-    .eq('student_id', email_student_id)
-    .eq('password', password)
+    const { email, password } = req.body
+    console.log('email', email)
+    console.log('password', password)
+    const user = await supabase.from('Users').select('student_id,email,password').eq('email', email).limit(1).single()
 
-  // If no user found, try to find a user with matching email
-  if (!data || data.length === 0) {
-    ;({ data, error } = await supabase
-      .from('Users')
-      .select('student_id,email,password')
-      .eq('email', email_student_id)
-      .eq('password', password))
-  }
+    // Check password
+    if (user?.data?.password !== password) {
+      return res.status(401).json({ message: 'Not found identity' })
+    }
 
-  if (error) {
-    console.error('Error:', error)
-    res.status(400).json({ error: 'Failed to login' })
-    return
-  }
+    // Sign JWT token
+    const accessToken = jwt.sign(
+      { student_id: user?.data?.student_id, email: user?.data?.email },
+      process.env.JWT_SECRET,
+      { expiresIn: '100h' }
+    )
 
-  if (data && data.length > 0) {
-    console.log('Login successful:', data)
-    res.status(200).json({ data })
-  } else {
-    console.log('No matching user found')
-    res.status(404).json({ error: 'No matching user found' })
+    const userData = {
+      student_id: user?.data?.student_id,
+      email: user?.data?.email
+    }
+    console.log('user?.data', user?.data)
+
+    res.status(200).json({ accessToken, user: userData })
+  } catch (error) {
+    console.log('error', error)
   }
 }
 
 export default handler
+
+// const handler = async (req: any, res: any) => {
+//   const { email_student_id, password } = req.body
+
+//   // First, try to find a user with matching student_id
+//   let { data, error } = await supabase
+//     .from('Users')
+//     .select('student_id,email,password')
+//     .eq('student_id', email_student_id)
+//     .eq('password', password)
+
+//   console.log('data', data)
+
+//   // If no user found, try to find a user with matching email
+//   if (!data || data.length === 0) {
+//     ;({ data, error } = await supabase
+//       .from('Users')
+//       .select('student_id,email,password')
+//       .eq('email', email_student_id)
+//       .eq('password', password))
+//   }
+
+//   if (error) {
+//     console.error('Error:', error)
+//     res.status(400).json({ error: 'Failed to login' })
+//     return
+//   }
+
+//   if (data && data.length > 0) {
+//     console.log('Login successful:', data)
+//     res.status(200).json({ data })
+//   } else {
+//     console.log('No matching user found')
+//     res.status(404).json({ error: 'No matching user found' })
+//   }
+// }
+
+// export default handler
