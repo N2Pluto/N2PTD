b9f838e004a9cbeb3b0fa77aa2090b93ee49b36b
Ponlawat Lueakaeo <76694369+h2ofriday@users.noreply.github.com>
2024-05-30 19:08:52 +0700

    fix:bug

--- src/@core/layouts/components/shared-components/footer/FooterContent.tsx
+++ src/@core/layouts/components/shared-components/footer/FooterContent.tsx
@@ -12,40 +12,16 @@ const FooterContent = () => {
   return (
     <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'space-between' }}>
       <Typography sx={{ mr: 2 }}>
-        {`© ${new Date().getFullYear()}, Made with `}
+        {`© ${new Date().getFullYear()}, Walailak University `}
         <Box component='span' sx={{ color: 'error.main' }}>
-          ❤️
+
         </Box>
         {` by `}
-        <Link target='_blank' href='https://themeselection.com/'>
-          ThemeSelection
+        <Link target='_blank' href='https://github.com/N2Pluto/N2PTD'>
+          PTD
         </Link>
       </Typography>
-      {hidden ? null : (
-        <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', '& :not(:last-child)': { mr: 4 } }}>
-          <Link
-            target='_blank'
-            href='https://github.com/themeselection/materio-mui-react-nextjs-admin-template-free/blob/main/LICENSE'
-          >
-            MIT License
-          </Link>
-          <Link target='_blank' href='https://themeselection.com/'>
-            More Themes
-          </Link>
-          <Link
-            target='_blank'
-            href='https://github.com/themeselection/materio-mui-react-nextjs-admin-template-free/blob/main/README.md'
-          >
-            Documentation
-          </Link>
-          <Link
-            target='_blank'
-            href='https://github.com/themeselection/materio-mui-react-nextjs-admin-template-free/issues'
-          >
-            Support
-          </Link>
-        </Box>
-      )}
+      
     </Box>
   )
 }
