c10d597f7ff63b04bfdfa959de77142269c0f1f5
Ponlawat Lueakaeo <76694369+h2ofriday@users.noreply.github.com>
2024-05-30 19:15:43 +0700

    fix:bug

--- src/pages/api/admin/renewalDormitory/read/fetchFormByDate.ts
+++ src/pages/api/admin/renewalDormitory/read/fetchFormByDate.ts
@@ -8,11 +8,7 @@ async function handler(req : any, res : any) {
     .select('*')
     .eq('user_id', user_id)
 
-  if (residentError || !residentData || residentData.length === 0) {
-    res.status(404).json({ error: 'User not found in Dormitory_Resident' })
 
-    return
-  }
 
   const { data, error } = await supabase
     .from('Renewal_System')
@@ -20,11 +16,7 @@ async function handler(req : any, res : any) {
     .eq('status', true)
     .order('id', { ascending: true })
 
-  if (error || !data || data.length === 0) {
-    res.status(500).json({ error: 'No data found or error occurred' })
-
-    return
-  }
+  
 
   res.status(200).json({ data, residentData })
 }
